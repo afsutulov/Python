874672e2213adbbd55b205c5f5ca7d6c3332fcda
afsutulov <glukosil@yandex.ru>
2025-04-07 19:52:02 +0500

    11

---
+++ main.py
@@ -0,0 +1,16 @@
+#!C:\Users\Mp\AppData\Local\Microsoft\WindowsApps\python3.13.exe
+
+# print('Hello world!!!\n')
+# Функция для печати текста и его вывода в консоль
+
+#name = input('Введите ваш имя: ')
+#age = input('Введите ваш возраст: ')
+
+#print(f'Привет, {name}! Тебе, {age} лет')
+
+# Калькузятор
+
+value1 = input("Введите первую переменную: ")
+value2 = input("Введите вторую переменную: ")
+
+print(f'Сумма: {int(value1) + int(value2)}')
