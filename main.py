57c82ad64ab8f1424acac8f65dc5a30329d88dd3
afsutulov <glukosil@yandex.ru>
2025-04-07 21:11:37 +0500

    111

--- main.py
+++ main.py
@@ -8,9 +8,31 @@
 
 #print(f'Привет, {name}! Тебе, {age} лет')
 
-# Калькузятор
+# Калькулятор
 
-value1 = input("Введите первую переменную: ")
-value2 = input("Введите вторую переменную: ")
+value1 = int(input("Введите первую переменную: "))
+value2 = int(input("Введите вторую переменную: "))
 
-print(f'Сумма: {int(value1) + int(value2)}')
+print(f'\nСумма: {value1 + value2}')
+print(f'Вычитание: {value1 - value2}')
+print(f'Умножение: {value1 * value2}')
+print(f'Деление целое: {value1 / value2}')
+print(f'Деление целая часть: {value1 // value2}')
+print(f'Деление остаток: {value1 % value2}')
+print(f'Возведение в степень: {value1 ** value2}')
+print("*" * 11) # одиназдать звездочек
+
+# Проверка и сравнение данных
+
+print(f'Первое значение больше второго: {value1 > value2}')
+print(f'Первое значение меньше второго: {value1 < value2}')
+print(f'Первое значение рано второму: {value1 == value2}')
+print(f'Первое значение не рано второму: {value1 != value2}')
+print(f'Первое значение равно "Hello World": {value1 == 'Hello World'}')
+
+
+
+# string - тип данных для текста
+# int - тип данных для целого числа
+# float - тип данных числа с плавающей точкой
+# bool - тип данных логический True / False
