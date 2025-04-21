29994bd30e92fc61d5b72d1902fd544e7efd7f09
Alexandr Sutulov <as@as-home.lan>
2025-04-21 22:36:37 +0500

    11

--- 2025-04-21/dz1.go
+++ 2025-04-21/dz1.go
@@ -2,23 +2,22 @@ package main
 
 import (
     "fmt"
-    "unicode"
     "strconv"
     "math/rand"
     "time"
 )
 
 // Функция ввоад числа с проверкой
-func Num(txt string) int {
+func Num(txt string, size int) int {
     var x string
     for {
-        fmt.Print(txt, ": ")
+        fmt.Printf("%s: ", txt)
         fmt.Scanln(&x)
-        Flag := true
-        for _, char := range x {
-            if !unicode.IsDigit(char) { Flag = false; break }
-        }
-        if Flag { y, _ := strconv.Atoi(x); return y } else { fmt.Println("Ошибка: Введенные данные не содержат чило!\n") }
+        if size == 0 || len(x) == size {
+           y, err := strconv.Atoi(x)
+           if err != nil { fmt.Println("Ошибка: Введенные данные не содержат чило!\n"); continue }
+           return y
+        } else { fmt.Printf("Ошибка: Число не %d значное!!\n", size) }
     }
 }
 
@@ -27,7 +26,7 @@ func main() {
 
     // Задание 1
     for {
-       val[0] = Num("Введите число")
+       val[0] = Num("Введите число до 10", 1)
        if val[0] < 10 { break } else { fmt.Print("Введите число меньше 10\n") }
     }
 
@@ -36,21 +35,21 @@ func main() {
     }
 
     // Задание 2
-    val[0] = Num("\nВведите сумму в рублях")
+    val[0] = Num("\nВведите сумму в рублях", 0)
     fmt.Println("\nКонвертировать:\n\t1: Доллары США\n\t2: Евро\n\t3: Казахстанские Тенге")
-    nm := Num("Введите нужное действие")
+    nm := Num("Введите нужное действие", 1)
     switch nm {
        case 1: fmt.Printf("Сумма в Долларах США: %d\n", val[0] * 85)
        case 2: fmt.Printf("Сумма в Евро: %d\n", val[0] * 105)
-       case 3: fmt.Printf("Сумма в Казахстанских Тенге: %d\n", val[0] * 5,15)
+       case 3: fmt.Printf("Сумма в Казахстанских Тенге: %d\n", int(float64(val[0]) * 5.15))
        default: fmt.Println("Ошибка! Вы ввели неправильное действие!\n")
     }
 
     // Задание 3
     for {
-       val[0] = Num("\nВведите нижнюю границу диапазона")
-       val[1] = Num("Введите верхнюю границу диапазона")
-       val[2] = Num("Введите число")
+       val[0] = Num("\nВведите нижнюю границу диапазона", 1)
+       val[1] = Num("Введите верхнюю границу диапазона", 1)
+       val[2] = Num("Введите число", 1)
        if val[2] >= val[0] && val[2] <= val[1] { break } else { fmt.Println("Введены некорректные данные, повторите попытку\n") }
     }
     for i := val[0]; i <= val[1]; i++ {
@@ -64,7 +63,7 @@ func main() {
     i := 0
     cur_time := time.Now()
     for {
-       val[0] = Num("Введите предполагаемое число")
+       val[0] = Num("\nВведите предполагаемое число", 0)
        if val[0] == 0 { break }
        i++
        if val[0] == rnd {
--- 2025-04-21/dz2.go
+++ 2025-04-21/dz2.go
@@ -2,73 +2,65 @@ package main
 
 import (
     "fmt"
-    "unicode"
     "strconv"
 )
 
 // Функция ввоад числа с проверкой
-func Num(txt string) int {
+func Num(txt string, size int) int {
     var x string
     for {
-        fmt.Print(txt, ": ")
+        fmt.Printf("%s: ", txt)
         fmt.Scanln(&x)
-        Flag := true
-        for _, char := range x {
-            if !unicode.IsDigit(char) { Flag = false; break }
-        }
-        if Flag { y, _ := strconv.Atoi(x); return y } else { fmt.Println("Ошибка: Введенные данные не содержат чило!\n") }
+        if size == 0 || len(x) == size {
+           y, err := strconv.Atoi(x)
+           if err != nil { fmt.Println("Ошибка: Введенные данные не содержат чило!\n"); continue }
+           return y
+        } else { fmt.Printf("Ошибка: Число не %d значное!!\n", size) }
     }
 }
 
 func main() {
-    var val = [3] int {0, 0, 0}
+    var val = [3] int {0, 0}
 
     // Задание 1
-    for {
-       val[0] = Num("Введите размер стороны квадрата (до 20)")
-       if val[0] <= 20 { break } else { fmt.Println("Ошибка: размер должен быть до 20!\n") }
-    }
+    val[0] = Num("Введите размер стороны квадрата", 0)
     fmt.Println("Результат:")
     for i := 0; i < val[0]; i++ {
        for j :=0; j < val[0]; j++ {
-           fmt.Printf("*")
+           fmt.Print("*")
        }
        fmt.Println()
     }
 
     // Задание 2
-    val[0] = Num("Введите ширину прямоугольника)")
-    val[1] = Num("Введите высоту прямоугольника)")
+    val[0] = Num("Введите ширину прямоугольника", 0)
+    val[1] = Num("Введите высоту прямоугольника", 0)
     fmt.Println("Результат:")
     for i := 0; i < val[1]; i++ {
        for j := 0; j < val[0]; j++ {
-           fmt.Printf("*")
+           fmt.Print("*")
        }
        fmt.Println()
     }
 
     // Задание 3
-    for {
-       val[0] = Num("Введите размер стороны квадрата (до 20)")
-       if val[0] <= 20 { break } else { fmt.Println("Ошибка: размер должен быть до 20!\n") }
-    }
+    val[0] = Num("Введите размер стороны квадрата", 0)
     fmt.Println("Результат:")
     for i := 1; i <= val[0]; i++ {
        for j :=1; j <= val[0]; j++ {
-           if i == 1 || i == val[0] || j == 1 || j == val[0] { fmt.Printf("*") } else { fmt.Printf(" ") }
+           if i == 1 || i == val[0] || j == 1 || j == val[0] { fmt.Print("*") } else { fmt.Print(" ") }
        }
        fmt.Println()
     }
 
     // Задание 4
-    val[0] = Num("Введите ширину прямоугольника)")
-    val[1] = Num("Введите высоту прямоугольника)")
+    val[0] = Num("Введите ширину прямоугольника", 0)
+    val[1] = Num("Введите высоту прямоугольника", 0)
     fmt.Println("Результат:")
     for i := 1; i <= val[1]; i++ {
        for j := 1; j <= val[0]; j++ {
-           if i == 1 || i == val[1] || j == 1 || j == val[0] { fmt.Printf("*") } else { fmt.Printf(" ") }
+           if i == 1 || i == val[1] || j == 1 || j == val[0] { fmt.Print("*") } else { fmt.Print(" ") }
        }
        fmt.Println()
     }
-
 }
