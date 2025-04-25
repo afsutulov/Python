f5c95370de0decbaff05531156e54ef8507e057c
Alexandr Sutulov <as@petropavl.kz>
2025-04-25 18:00:17 +0500

    11

--- 2025-04-23/KrestikiNolikiConsole.py
+++ 2025-04-23/KrestikiNolikiConsole.py
@@ -1,52 +1,36 @@
 #!/usr/bin/python3
 
+import os
+
 board = [' '] * 9 # игровая доска
 player = 'X' # Ход игрока
-game_running = True # Условие для игры
-winner = None
-winList = [[0,1,2],[3,4,5],[6,7,8],
-           [0,3,6],[1,4,7],[2,5,8],
-           [0,4,8],[2,4,6]] # Список комбанаций для победы
-
-def Doska(Flag):
-    for _ in range(3):
-       print('-' * 9)
-       if Flag: print(f'{board[_*3]} | {board[_*3+1]} | {board[_*3+2]}')
-       else: print(f'{_*3+1} | {_*3+2} | {_*3+3}')
+winList = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]] # Выигрышные комбинации
 
 # Вывод игрового поля
-print('Добро пожаловать в игру: Крестики-Нолики!')
-print('Играем на поле с выбором клеток:')
-Doska(False)
-print('\nУдачи и хорошей игры!')
+def Doska():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('Консольная игра \"Крестики-Нолики\"!')
+    print('\n  ПОЛЕ   \t\t НУМЕРАЦИЯ')
+    for _ in range(3):
+       print('-' * 9, '\t\t', '-' * 9)
+       print(f'{board[_*3]} | {board[_*3+1]} | {board[_*3+2]}\t\t {_*3+1} | {_*3+2} | {_*3+3}')
+    print('-' * 9, '\t\t', '-' * 9, f'\n\nХОД ИГРОКА: {player}')
+
+# Функция ввода числа с проверкой
+def Num(txt):
+    while True:
+        _ = input(f'{txt}: ')
+        if _.isnumeric():
+           if 0 < int(_) < 10: return int(_)
+           else: print(f'Ошибка: Введите позицию от 1 до 9!\n')
+        else: print('Ошибка: Введенные данные не содержат чило!\n')
 
 # Игровой процесс
-while game_running:
-    # Ход игрока
-    Doska(True)
-    print(f'Ход игрока: {player}')
-    while True:
-       try:
-          position = int(input('Веберете позицию для хода (1-9): ')) - 1
-          if 0 <= position <= 8 and board[position] == ' ': break
-          else: print('Некорректный ход. Обдумайте еще раз!')
-       except ValueError:
-          print('Попробуйте ввести числов от 1-9!!!')
-    board[position] = player
-    # Проверка на победителя
-    for combo in winList:
-        if board[combo[0]] == board[combo[1]] == board[combo[2]] != ' ':
-           winner = player
-           game_running = False
-           break
-    # Проверка на ничью
-    if ' ' not in board: game_running = False
-    if game_running:
-       if player == 'X': player = '0'
-       else: player = 'X'
-
-# Вывести финальную доску
-print('\nФинальная доска')
-Doska(True)
-if winner: print(f'Победил игрок: {winner}!!!')
-else: print('Игра закончилась ничьей!')
+while True:
+    Doska()
+    board[Num('Выберите позицию для хода') - 1] = player
+    for _ in winList:
+        if board[_[0]] == board[_[1]] == board[_[2]] != ' ': Doska(); print(f'\n\nИГРА ОКОНЧЕНА!\nПобедил игрок: {player}'); exit()
+    if ' ' not in board: Doska(); print('\n\nИГРА ОКОНЧЕНА!\nУ вас ничья!'); exit()
+    if player == 'X': player = '0'
+    else: player = 'X'
