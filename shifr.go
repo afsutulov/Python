8f154833daaba31168e3d7cf3de2ab95e710ded0
Alexandr Sutulov <as@as-home.lan>
2025-05-17 17:30:05 +0500

    11

--- shifr.go
+++ shifr.go
@@ -12,7 +12,7 @@ func main() {
     data, err := ioutil.ReadFile(args[1])
     if err != nil { return }
     for i := 0; i < len(data); i ++ {
-       if i % 10 < key[i % 10] && i / 10 + key[i % 10] <= len(data) { data[i], data[i / 10 * 10 + key[i % 10]] = data[i / 10 * 10  + key[i % 10]], data[i] }
+       if i % 10 < key[i % 10] && i / 10 * 10 + key[i % 10] <= len(data){ data[i], data[i / 10 * 10 + key[i % 10]] = data[i / 10 * 10  + key[i % 10]], data[i] }
        data[i] = 255 - data[i]
     }
     err = ioutil.WriteFile(args[1], data, 0644)
